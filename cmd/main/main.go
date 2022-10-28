8af40834d16e60f8227e62d7d5002f6314752f7d
SpikeJus <73031760+SpikeJus@users.noreply.github.com>
2022-10-28 20:38:07 +0300

    Created CRUD API with MySQL connection

---
+++ cmd/main/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"mysql_storage/pkg/routes"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
